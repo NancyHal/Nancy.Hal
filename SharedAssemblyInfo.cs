497f3106fc3f676ce3a1a046994c2e4c9ecd70bf
Luke Jones <luke.jones@globalx.com.au>
2017-12-18 12:39:12 +1000

    Add circleci

--- SharedAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("Nancy.Hal")]
-[assembly: AssemblyDescription("Provides Hal+JSON media type support for Nancy.")]
-[assembly: AssemblyCompany("Dan Barua, Matt Vane, Thomas Eizinger, Richard Bennett")]
-[assembly: AssemblyProduct("Nancy.Hal")]
-[assembly: AssemblyCopyright("Copyright (C) Dan Barua, Matt Vane, Thomas Eizinger, Richard Bennett and other contributors")]
-[assembly: AssemblyVersion("2.0.0")]
